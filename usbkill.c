2428066f05976497ae6bc4baddc0d71235535dbf
aeidi <45346433+aeidi@users.noreply.github.com>
2018-11-28 12:38:58 +0900

    Update usbkill.c

--- usbkill.c
+++ usbkill.c
@@ -1,25 +1,24 @@
 #define pr_fmt(fmt)	KBUILD_MODNAME ": " fmt
-	// pr_fmt(fmt)  ==> KBUILD_MODNAME ": " fmt 로 정의
+	// pr_fmt(fmt)  ==> KBUILD_MODNAME ": " fmt로 정의
 	// pr_info("") 을 쓰기 위한 정의
 #define N_ELEMENTS(array) (sizeof(array)/sizeof((array)[0]))
-	// N_ELEMENTS(array) ==> (sizeof(array)/sizeof((array)[0])) 로 변환
-	// array 전체 사이즈 / 한 칸의 사이즈
+	// N_ELEMENTS(array) ==> (sizeof(array)/sizeof((array)[0]))로 정의
 
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/usb.h>
 #include <linux/reboot.h>
-#include "config.h"
+#include "config.h" // Whitelist
 
 MODULE_LICENSE("GPL");
 
 /*
  * returns 0 if no match, 1 if match
- * 매치가 되면 리턴 0, 매치되면 리턴 1
+ *
  * Taken from drivers/usb/core/driver.c
- *   => 리눅스의 usb 관련 소스코드.
  */
+
 static int usb_match_device(struct usb_device *dev, const struct usb_device_id *id){
 		//인자값으로 usb_device dev, usb_device_id id. 포인터
 	if ((id->match_flags & USB_DEVICE_ID_MATCH_VENDOR) &&
@@ -30,6 +29,7 @@ static int usb_match_device(struct usb_device *dev, const struct usb_device_id *
 
 	/* No need to test id->bcdDevice_lo != 0, since 0 is never
 	   greater than any unsigned number. */
+	
 	if ((id->match_flags & USB_DEVICE_ID_MATCH_DEV_LO) &&
 			(id->bcdDevice_lo > le16_to_cpu(dev->descriptor.bcdDevice))) { return 0; }
 
@@ -54,24 +54,7 @@ static void kill(struct usb_device *usb){	//kill 함수. kernel 종료시키는 
 
 	// for문
 	for (dev = &usb->dev; dev; dev = dev->parent){	// 현재 usb부터 dev 전체를 for문으로
-		mutex_unlock(&dev->mutex); //mutex = 잠금. 락.
-		/*
-		Name
-			mutex_unlock — release the mutex  mutex 해제. 잠금해제
-		Synopsis
-			void __sched mutex_unlock (	struct mutex * lock);
- 
-		Arguments
-			struct mutex * lock		해제될 잠금을 인자값으로 받는다.
-				the mutex to be released
-		Description
-			Unlock a mutex that has been locked by this task previously.
-				잠겨있는 mutex를 잠금 해제한다.
-			This function must not be used in interrupt context. Unlocking of a not locked mutex is not allowed.
-				이 함수는 정지된 context에서 사용할 수 없다. 잠금되지 않은 mutex를 해제하는 것은 안된다.
-			This function is similar to (but not equivalent to) up.
-				이 함수는 up과 비슷하다.
-		*/
+		mutex_unlock(&dev->mutex);
 	}
 	printk("Powering off.\n");	//커널에 Powering off 출력
 	kernel_power_off();	// 커널 종료.
